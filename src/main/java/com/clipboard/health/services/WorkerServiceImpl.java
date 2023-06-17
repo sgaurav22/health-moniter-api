1e45a533ab2a553146ce87b4cd08abc9299dc9f5
Gaurav Saxena <saxenagau@gmail.com>
2023-06-17 16:09:35 +0530

    #2 : Added annotation to class.

--- src/main/java/com/clipboard/health/services/WorkerServiceImpl.java
+++ src/main/java/com/clipboard/health/services/WorkerServiceImpl.java
@@ -7,11 +7,13 @@ import com.clipboard.health.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class WorkerServiceImpl implements WorkerService {
 
     @Autowired
