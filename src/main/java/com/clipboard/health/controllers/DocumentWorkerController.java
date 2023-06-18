6206053863521afb0c2c3fb804116c8ee472f52c
Gaurav Saxena <saxenagau@gmail.com>
2023-06-18 08:17:56 +0530

    #6 : Added POST in DocumentWorkerController for REST API.

--- src/main/java/com/clipboard/health/controllers/DocumentWorkerController.java
+++ src/main/java/com/clipboard/health/controllers/DocumentWorkerController.java
@@ -1,19 +1,21 @@
 package com.clipboard.health.controllers;
 
 import com.clipboard.health.domains.DocumentWorker;
+import com.clipboard.health.exceptions.ClipboardException;
 import com.clipboard.health.services.DocumentWorkerService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -41,6 +43,26 @@ public class DocumentWorkerController {
     public ResponseEntity<List<DocumentWorker>> findAll(@PathVariable Integer offset, @PathVariable Integer limit) {
         List<DocumentWorker> list = documentWorkerService.findAll(offset, limit);
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @PostMapping("/documentWorker")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
+    public ResponseEntity<List<DocumentWorker>> create(@RequestBody List<DocumentWorker> documentWorkers) {
+        List<DocumentWorker> list = new ArrayList<>();
+        try {
+            if (Objects.nonNull(documentWorkers)) {
+                Iterable<DocumentWorker> iterable = documentWorkerService.saveAll(documentWorkers);
+                iterable.forEach(list::add);
+            }
+        } catch (Exception e) {
+            logger.error("Error occur while saving documentWorkers", e);
+            throw new ClipboardException("Error occur while saving documentWorkers");
+        }
+        logger.info("DocumentWorker created successfully.", list);
+        return new ResponseEntity<>(list, HttpStatus.CREATED);
     }
 
 
